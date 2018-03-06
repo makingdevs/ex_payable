da58d2b8a9b728d69c115654daea376f15e00e91
cggg88jorge <jorge@makingdevs.com>
2018-03-06 15:50:50 -0600

    #2 Documentando la parte de costumers para la llamada a el api de open pay

--- lib/ex_payable/customers.ex
+++ lib/ex_payable/customers.ex
@@ -1,7 +1,20 @@
 defmodule ExPayable.Customers do
+  @moduledoc """
+  Functions for working with customers at Openpay. Through this API you can:
+
+    * get a customer
+
+  """
 
   @endpoint "customers"
 
+  @doc """
+   Get a customer.
+   Gets a customer for given owner using customer ID.
+   Returns a `{:ok, customer}` tuple.
+   ## Examples
+       {:ok, customer} = ExOpenpay.Customers.get(customer_id)
+   """
   def get(id) do
     ExPayable.make_request(:get, "#{@endpoint}/#{id}")
   end
