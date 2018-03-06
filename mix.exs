16601dca6a804b41a8e31ec19903f46c71117147
cggg88jorge <jorge@makingdevs.com>
2018-03-06 12:00:16 -0600

    #1 Ajustando la configuracion de la aplicacion en el mix

--- mix.exs
+++ mix.exs
@@ -13,9 +13,8 @@ defmodule ExPayable.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger, :httpoison]
-    ]
+    [ applications: [:httpotion] ]
+    [extra_applications: [:logger]]
   end
 
   # Run "mix help deps" to learn about dependencies.
