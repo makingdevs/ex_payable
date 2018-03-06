7543f19cc429015f8e93793994b4673fe013164b
cggg88jorge <jorge@makingdevs.com>
2018-03-06 15:51:42 -0600

    #2 Afinando el llamado al api y ajustando variables externalizadas como la url

--- lib/ex_payable.ex
+++ lib/ex_payable.ex
@@ -7,7 +7,7 @@ defmodule ExPayable do
   defmodule MissingSecretKeyError do
     defexception message: """
       The merchant_id setting is required so that we can report the
-      correct environment instance to ExOpenpay. Please configure
+      correct environment instance to ExPayable. Please configure
       merchant_id in your config.exs and environment specific config files
       to have accurate reporting of errors.
       config :ex_openpay, merchant_id: YOUR_MERCHANT_ID
@@ -17,30 +17,35 @@ defmodule ExPayable do
   defmodule MissingApiKeyError do
     defexception message: """
       The api_key setting is required so that we can report the
-      correct environment instance to ExOpenpay. Please configure
+      correct environment instance to ExPayable. Please configure
       api_key in your config.exs and environment specific config files
       to have accurate reporting of errors.
       config :ex_openpay, api_key: YOUR_API_KEY
     """
   end
 
-  def process_url(url) do
-    "https://sandbox-api.openpay.mx/v1/" <> url
+  defmodule MissingApiBaseUrlError do
+    defexception message: """
+      The api_base_url setting is required so that we can report the
+      correct environment instance to ExPayable. Please configure
+      api_base_url in your config.exs and environment specific config files
+      to have accurate reporting of errors.
+      config :ex_openpay, api_base_url: YOUR_API_BASE_URL
+    """
+  end
+
+  def process_url(endpoint) do
+    "https://#{openpay_api_key()}:@#{openpay_api_base_url()}/v1/#{openpay_merchant_id()}/" <> endpoint
   end
 
   def process_response_body(body) do
     body
     |> Poison.decode!
-    #|> Map.take(@expected_fields)
-    #|> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
   end
 
   def req_headers(key) do
     Map.new
       |> Map.put("Content-Type",  "application/json")
-      |> Map.put("Accept", "application/json; charset=utf-8")
-      |> Map.put("Authorization", "Basic #{key}")
-      |> Map.put("User-Agent",    "ExOpenpay/v1 openpay/1.4.0")
   end
 
   def make_request(method, endpoint, body \\ %{}, headers \\ %{}, options \\ []) do
@@ -48,17 +53,12 @@ defmodule ExPayable do
   end
 
   def make_request_with_key( method, endpoint, key, body \\ %{}, headers \\ %{}, options \\ []) do
-    #rb = ExOpenpay.URI.encode_query(body)
-    IO.puts key
-    rh = req_headers(key)
+    requestHeader = req_headers(key)
         |> Map.merge(headers)
         |> Map.to_list
-    IO.inspect rh
-    #options = Keyword.merge(httpoison_request_options(), options)
     {:ok, response} =
       case method do
-        #:delete -> HTTPoison.delete(process_url(endpoint))
-        _ -> request(method, endpoint, "", rh, options)
+        _ -> request(method, endpoint, "", requestHeader, options)
       end
       response.body
   end
@@ -83,6 +83,18 @@ defmodule ExPayable do
     case Application.get_env(:ex_payable, :api_key, System.get_env "OPENPAY_API_KEY") || :not_found do
       :not_found ->
         raise MissingApiKeyError
+      value -> value
+    end
+  end
+
+  @doc """
+  Get the :api_base_url from system env
+  if not exist raise a MissingApiBaseUrlError exception
+  """
+  def openpay_api_base_url do 
+    case Application.get_env(:ex_payable, :api_base_url, System.get_env "OPENPAY_API_BASE_URL") || :not_found do
+      :not_found ->
+        raise MissingApiBaseUrlError
       value -> value
     end
   end
