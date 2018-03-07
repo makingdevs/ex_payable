019b31d4ad6653170156f83a28a2e40091a50088
cggg88jorge <jorge@makingdevs.com>
2018-03-07 12:07:37 -0600

    #3 Creando util para funciones en general

---
+++ lib/ex_payable/util.ex
@@ -0,0 +1,35 @@
+defmodule ExPayable.Util do
+
+  def string_map_to_atoms([string_key_map]) do
+    string_map_to_atoms string_key_map
+  end
+
+  def string_map_to_atoms(string_key_map) do
+    for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), val}
+  end
+
+  def handle_openpay_response(res) do
+    cond do
+      res["error_code"] -> {:error, res}
+      true -> {:ok, ExPayable.Util.string_map_to_atoms res}
+    end
+  end
+
+  @doc """
+  Takes a keyword list and turns it into proper query values.
+
+  ## Example
+  card_data = [
+    name: "customer name",
+    last_name: "customer last name",
+    email: "customer@email.com",
+    phone_number: "",
+  ]
+
+  ExPayable.URI.encode_query(card) # "{\"phone_number\":\"\",\"name\":\"customer name\",\"last_name\":\"customer last name\",\"email\":\"customer@email.com\"}"
+  """
+  def encode_query(list) do
+    Poison.encode!(Enum.into(list, %{}))
+  end
+
+end
